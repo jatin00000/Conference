4ab28703f36053cc645020ce021c4c388113ea2d
jatin00000 <jatin.k0jatin@gmail.com>
2023-06-17 19:34:20 +0530

    Removed Jot Form

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
-import React, {useState} from 'react';
-import {HashRouter, Route, Routes } from 'react-router-dom'
-import Footer from './components/Footer'
-import Navbar from'./components/navbar'
-import Home from'./components/Home'
-import Abstract from'./components/Abstract'
-import Register from'./components/Register'
+import React, { useState } from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/navbar";
+import Home from "./components/Home";
 
-
-import './index.css'
+import "./index.css";
 function App() {
-      const [show, setShow] = useState(true);
-      const func = () => {
-        setShow(show => !show)
-      }
+  const [show, setShow] = useState(true);
+  const func = () => {
+    setShow((show) => !show);
+  };
   return (
-
-    
-
-      
-      
-      
-          <HashRouter>
-            <Navbar func={func}/>
-            <Routes>
-    
-
-
-          <Route exact path='/' element={show && <Home/>} />
-<Route exact  path='/register' element={show &&  <Register/>} />
-  <Route exact path='/abstract' element={show && <Abstract/>} />
-      
-      
-    
-</Routes>
-<Footer show={show}/>
+    <HashRouter>
+      <Navbar func={func} />
+      <Routes>
+        <Route exact path="/" element={show && <Home />} />
+      </Routes>
+      <Footer show={show} />
     </HashRouter>
- 
-    
   );
 }
 
-
 export default App;
-
